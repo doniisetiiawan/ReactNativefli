e481029bd3af376acd6cacdb2c0b894dbe7987d3
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-21 15:01:09 +0700

    Inline styles

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import InlineStyles from './src/InlineStyles';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>
-        Open up App.js to start working on your app!
-      </Text>
-    </View>
-  );
+  return <InlineStyles />;
 }
