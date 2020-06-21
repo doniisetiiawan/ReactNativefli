6f2162b42c4ea1229a7bb2c284d93627975d25e1
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-21 15:23:02 +0700

    Covering the other axis

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import BoxModelDemo from './src/BoxModelDemo';
+import FlexBoxLayout from './src/FlexBoxLayout';
 
 export default function App() {
-  return <BoxModelDemo />;
+  return <FlexBoxLayout />;
 }
---
+++ src/FlexBoxLayout.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+  },
+  item: {
+    backgroundColor: 'lightgoldenrodyellow',
+    borderWidth: 1,
+    borderColor: 'goldenrod',
+    height: 150,
+    width: 150,
+  },
+});
+
+const FlexBoxLayout = () => (
+  <View style={styles.container}>
+    <View
+      style={[styles.item, { alignSelf: 'flex-start' }]}
+    />
+    <View
+      style={[
+        styles.item,
+        { alignSelf: 'stretch', width: undefined },
+      ]}
+    />
+    <View
+      style={[styles.item, { alignSelf: 'flex-end' }]}
+    />
+  </View>
+);
+
+export default FlexBoxLayout;
